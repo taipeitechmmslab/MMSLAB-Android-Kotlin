291c7e734a243836a57c4651cb1840105d387550
MAI GUANG-TING <a70325000@gmail.com>
2024-06-26 14:14:19 +0800

    feat: update

--- Lab13/app/src/main/java/com/example/lab13/MainActivity.kt
+++ Lab13/app/src/main/java/com/example/lab13/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.lab13
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var batteryReceiver: MyReceiver
+    // 建立 BroadcastReceiver 物件
+    private val receiver =
+        object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                // 接收廣播後，解析 Intent 取得字串訊息
+                intent.extras?.let {
+                    val tvMsg = findViewById<TextView>(R.id.tvMsg)
+                    tvMsg.text = "${it.getString("msg")}"
+                }
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +37,37 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        batteryReceiver = MyReceiver()
+        findViewById<Button>(R.id.btnMusic).setOnClickListener {
+            register("music")
+        }
 
-        // 註冊廣播接收器
-        val filter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-        registerReceiver(batteryReceiver, filter)
+        findViewById<Button>(R.id.btnNew).setOnClickListener {
+            register("new")
+        }
+
+        findViewById<Button>(R.id.btnSport).setOnClickListener {
+            register("sport")
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        // 取消註冊廣播接收器
-        unregisterReceiver(batteryReceiver)
+        // 解除註冊廣播接收器
+        unregisterReceiver(receiver)
+    }
+
+    private fun register(channel: String) {
+        // 建立 IntentFilter 物件來指定接收的頻道
+        val intentFilter = IntentFilter(channel)
+        // 註冊廣播接收器
+        // 如果是 Android 12 以上的版本，則需要加上 RECEIVER_EXPORTED
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(receiver, intentFilter, RECEIVER_EXPORTED)
+        } else {
+            registerReceiver(receiver, intentFilter)
+        }
+        // 建立 Intent 物件，使其夾帶頻道資料，並啟動 MyService 服務
+        val i = Intent(this, MyService::class.java)
+        startService(i.putExtra("channel", channel))
     }
 }
