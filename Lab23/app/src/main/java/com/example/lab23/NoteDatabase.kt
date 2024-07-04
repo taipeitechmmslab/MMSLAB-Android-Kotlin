726d2799e5d0d64285b82f7508523a82dfb2385c
DDPlay123 <a70325000@gmail.com>
2024-07-05 02:31:24 +0800

    feat: update

--- Lab23/app/src/main/java/com/example/lab23/NoteDatabase.kt
+++ Lab23/app/src/main/java/com/example/lab23/NoteDatabase.kt
@@ -38,19 +38,31 @@ interface NoteDao {
     suspend fun updateNote(note: Note)
 }
 
-@Database(entities = [Note::class], version = 1)
+@Database(
+    // 定義資料表類別
+    entities = [Note::class],
+    // 定義資料庫版本
+    version = 1
+)
 abstract class NoteDatabase : RoomDatabase() {
+    // 定義抽象方法，用來取得 DAO 操作介面
     abstract fun noteDao(): NoteDao
 
+    // 定義靜態方法，用來取得資料庫實例
     companion object {
+        // 定義資料庫實例變數
         @Volatile
         private var instance: NoteDatabase? = null
 
+        // 定義靜態方法，用來取得資料庫實例
         fun getDatabase(context: Context): NoteDatabase =
+            // 使用雙重鎖定機制，確保資料庫實例只會被建立一次
             instance ?: synchronized(this) {
+                // 若資料庫實例為null，則建立資料庫實例
                 instance ?: buildDatabase(context).also { instance = it }
             }
 
+        // 定義靜態方法，用來建立資料庫實例
         private fun buildDatabase(context: Context) =
             Room.databaseBuilder(
                 context.applicationContext,
