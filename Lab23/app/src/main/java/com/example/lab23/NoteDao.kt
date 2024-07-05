37f6b0218e3632808e6d0422387c270af1756068
MAI GUANG-TING <a70325000@gmail.com>
2024-07-05 11:44:35 +0800

    feat: update

---
+++ Lab23/app/src/main/java/com/example/lab23/NoteDao.kt
@@ -0,0 +1,24 @@
+package com.example.lab23
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NoteDao {
+    @Query("SELECT * FROM notes ORDER BY timestamp DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+}
--- Lab23/app/src/main/java/com/example/lab23/NoteDatabase.kt
+++ Lab23/app/src/main/java/com/example/lab23/NoteDatabase.kt
@@ -1,42 +1,9 @@
 package com.example.lab23
 
 import android.content.Context
-import androidx.lifecycle.LiveData
-import androidx.room.Dao
 import androidx.room.Database
-import androidx.room.Delete
-import androidx.room.Entity
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.PrimaryKey
-import androidx.room.Query
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.Update
-
-@Entity(tableName = "notes")
-data class Note(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int,
-    val title: String,
-    val content: String,
-    val timestamp: Long
-)
-
-@Dao
-interface NoteDao {
-    @Query("SELECT * FROM notes ORDER BY timestamp DESC")
-    fun getAllNotes(): LiveData<List<Note>>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertNote(note: Note)
-
-    @Delete
-    suspend fun deleteNote(note: Note)
-
-    @Update
-    suspend fun updateNote(note: Note)
-}
 
 @Database(
     // 定義資料表類別
